672579468f946fcc25cd9dc3c7e7f33dac9e9650
saurabhh-svg <singhsaurabh2901@gmail.com>
2022-07-08 02:33:27 +0530

    About done

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,55 @@
 import React from "react";
 import "./about.css";
+import ME from "../../assets/me-about.jpg";
+import { FaAward } from "react-icons/fa";
+import { FiUsers } from "react-icons/fi";
+import { VscFolderLibrary } from "react-icons/vsc";
 const About = () => {
-  return <section id="about">About</section>;
+  return (
+    <section id="about">
+      <h5>Get to Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="About Image"></img>
+          </div>
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className="about__icon" />
+              <h5> Experience</h5>
+              <small>0+ years Working</small>
+            </article>
+
+            <article className="about__card">
+              <FiUsers className="about__icon" />
+              <h5> Clients</h5>
+              <small>10+ worldwide</small>
+            </article>
+
+            <article className="about__card">
+              <VscFolderLibrary className="about__icon" />
+              <h5>Projects</h5>
+              <small>4+ Completed</small>
+            </article>
+          </div>
+          <p>
+            lorem epsimfndvs vs v v sdvbjksbvjksbv dsvdsv ds vnsbvbkv sdv sd vds
+            hv lorem300 dkvdnsk vmdknv
+          </p>
+
+          <a href="contact" className="btn btn-primary">
+            {" "}
+            Let's Talk
+          </a>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
