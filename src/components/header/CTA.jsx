996dfad34e84fc47f59ce8f02d55057bd86e5890
saurabhh-svg <singhsaurabh2901@gmail.com>
2022-07-31 21:55:10 +0530

    CV update

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import CV from "../../assets/cv.pdf";
+import CV from "../../assets/Saurabh Singh-Resume.pdf";
 const CTA = () => {
   return (
     <div className="cta">
