9b120555ee6c2050c5b2afc048158fe77c6b476a
saurabhh-svg <singhsaurabh2901@gmail.com>
2022-08-24 00:49:43 +0530

    ProfilePic Update

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./header.css";
 import CTA from "./CTA";
-import ME from "../../assets/123.jpg";
+import ME from "../../assets/profile-pic (3).png";
 import HeaderSocials from "./HeaderSocials";
 
 const Header = () => {
